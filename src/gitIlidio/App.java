6b3fad2adf8d05e54def3709b5cfd6497ad09c0e
Rig <Rig@DESKTOP-FM58486.local>
2018-05-09 19:32:13 -0300

    Versão 1.0

---
+++ src/gitIlidio/App.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitIlidio;
+
+/**
+ *
+ * @author aluno
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Primeiro teste git versao 2.0");
+        // TODO code application logic here
+    }
+    
+}
